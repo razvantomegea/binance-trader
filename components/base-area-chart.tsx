382ea6472d7f190097eba57a5ceaabc07c8cd453
Razvan Tomegea <razvan.tomegea@gmail.com>
2026-05-26 07:29:38 +0300

    Update BaseAreaChartProps to allow any data type for the data property, enhancing flexibility in data handling.

--- components/base-area-chart.tsx
+++ components/base-area-chart.tsx
@@ -11,7 +11,8 @@ import {
 } from "recharts";
 
 interface BaseAreaChartProps {
-  data: Record<string, unknown>[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Record<string, any>[];
   dataKey: string;
   color: string;
   gradientId: string;
